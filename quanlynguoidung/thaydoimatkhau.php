7a26df5911c970244a9d528295b7f1028de49b2a
pthquynh-06 <quynhphanthihong@gmail.com>
2025-12-11 20:01:52 +0700

    bao vệ trang đăng nhập mật khẩu

--- quanlynguoidung/thaydoimatkhau.php
+++ quanlynguoidung/thaydoimatkhau.php
@@ -1,2 +1,10 @@
+<?php
+session_start();
+
+// BẢO VỆ TRANG (AC07 - Bảo mật trang)
+if(!isset($_SESSION['loggedin']) || !$_SESSION['loggedin']){
+    header("Location: dangnhaphethong.php");
+    exit;
+}
 
 

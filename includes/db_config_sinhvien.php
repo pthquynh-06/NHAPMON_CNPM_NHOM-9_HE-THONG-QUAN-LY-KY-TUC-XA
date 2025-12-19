879af99eb4d71e07d8753fea5c16f8a6ee843775
quynhanh-2711 <quinhanh.2711@gmail.com>
2025-12-19 10:22:08 +0700

    them ket noi sinh vien

---
+++ includes/db_config_sinhvien.php
@@ -0,0 +1,12 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+$conn = new mysqli("localhost", "root", "", "quanlysinhvien");
+$conn->set_charset("utf8");
+
+if ($conn->connect_error) {
+    die("Kết nối DB thất bại: " . $conn->connect_error);
+}
+?>

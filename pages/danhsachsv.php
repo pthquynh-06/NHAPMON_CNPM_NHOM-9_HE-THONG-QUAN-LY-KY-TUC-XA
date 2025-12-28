8e6daae05a80b6ad1da39d5c692b4e6cdb978446
quynhanh-2711 <quinhanh.2711@gmail.com>
2025-12-28 11:11:16 +0700

    lap trinh backend tim kiem phong theo tu khoa

--- pages/danhsachsv.php
+++ pages/danhsachsv.php
@@ -15,7 +15,7 @@ $sql = "SELECT * FROM sinhvien ORDER BY CAST(SUBSTRING(mssv, 3) AS UNSIGNED) ASC
 // --- XỬ LÝ TÌM KIẾM ---
 $search = $_GET['search'] ?? ''; 
 if (!empty($search)) {
-    $sql = "SELECT * FROM sinhvien WHERE hoten LIKE ? OR mssv LIKE ? OR sophong LIKE ? OR truong LIKE ? ORDER BYCAST(SUBSTRING(mssv, 3) AS UNSIGNED) ASC";
+    $sql = "SELECT * FROM sinhvien WHERE hoten LIKE ? OR mssv LIKE ? OR sophong LIKE ? OR truong LIKE ? ORDER BY CAST(SUBSTRING(mssv, 3) AS UNSIGNED) ASC";
     $stmt = $conn->prepare($sql);
     $searchTerm = "%$search%";
     $stmt->bind_param("ssss", $searchTerm, $searchTerm, $searchTerm, $searchTerm);
